92ad872486c25516cf40cb0cc52715332a5183bb
ungraduate_coder <120706974+flarebiltv09@users.noreply.github.com>
2023-02-11 08:27:06 +0000

    Main

--- script.js
+++ script.js
@@ -1,138 +1,277 @@
-class ValentineHearts extends HTMLElement {
-    #shadowRoot;
+var canvas = document.getElementById("canvas");
 
-    constructor() {
-        super();
-        this.#createShadowRoot();
-    }
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-    #createShadowRoot() {
-        this.#shadowRoot = this.attachShadow({ mode: 'open' });
-        this.#shadowRoot.innerHTML = `
-            <style>
-                :host {
-                    --heart-size: 1em;
-                    --heart-color: red;
-                    --heart-speed: 30s;
-
-                    pointer-events: none;
-                    display: block;
-                    position: absolute;
-                    top: 0;
-                    left: 0;
-                    width: 100%;
-                    height: 100%;
-                    z-index: 10000;
-                    overflow: hidden;
-                }
-
-                heart {
-                    opacity: 0;
-                    display: block;
-                    width: var(--heart-size);
-                    height: var(--heart-size);
-                    position: absolute;
-                    left: calc(var(--heart-x-offset) - 4%);
-                    background: var(--heart-color);
-                    transform: rotate(45deg) scale(0);
-                    transition: top var(--heart-speed) linear,
-                                opacity 1s ease-in-out,
-                                transform 1s ease-in-out;
-                    animation-duration: 6s;
-                    animation-iteration-count: infinite;
-                    will-change: top, left, opacity, transform;
-                }
-
-                heart::before {
-                    content: ' ';
-                    display: block;
-                    width: var(--heart-size);
-                    height: var(--heart-size);
-                    border-radius: calc(var(--heart-size) / 2);
-                    position: absolute;
-                    top: 0;
-                    left: calc(var(--heart-size) * -0.5);
-                    background: var(--heart-color);
-                }
-
-                heart::after {
-                    content: ' ';
-                    display: block;
-                    width: var(--heart-size);
-                    height: var(--heart-size);
-                    border-radius: calc(var(--heart-size) / 2);
-                    position: absolute;
-                    top: calc(var(--heart-size) * -0.5);
-                    left: 0;
-                    background: var(--heart-color);
-                }
-
-                @keyframes sway {
-                    0% {
-                        left: calc(var(--heart-x-offset) - 4%);
-                    }
-
-                    50% {
-                        left: calc(var(--heart-x-offset) + 4%);
-                    }
-
-                    100% {
-                        left: calc(var(--heart-x-offset) - 4%);
-                    }
-                }
-            </style>
-        `;
-    }
-
-    connectedCallback() {
-        setInterval(() => this.#spawnHeart(), 1000);
-
-        for (let i = 0; i < 20; i++) {
-            this.#spawnHeart(Math.round(Math.random() * 100));
-        }
-
-        this.addEventListener('resize', () => this.#resize());
-        this.#resize();
-    }
-
-    #resize() {
-        this.style.width = `${window.clientWidth}px`;
-        this.style.height = `${document.body.clientHeight}px`;
-    }
-
-    #spawnHeart(top) {
-        const heart = document.createElement('heart');
-
-        heart.style.top = `${top || Math.round(Math.random() * 25 + 75)}%`;
-        heart.style.setProperty('--heart-x-offset', `${Math.random() * 100}%`);
-
-        const brightness = Math.random();
-        heart.style.setProperty(
-            '--heart-color',
-            `rgb(255, ${Math.round(brightness * 150)}, ${Math.round(brightness * 50)})`
-        );
-
-        this.#shadowRoot.appendChild(heart);
-
-        setTimeout(() => {
-            heart.style.animationName = 'sway';
-        }, Math.random() * 3000);
-
-        setTimeout(() => {
-            heart.style.top = '-2%';
-            heart.style.opacity = 0.5;
-            heart.style.transform = `rotate(45deg) scale(${Math.random() * 0.4 + 0.8})`;
-
-            function removeHeart(event) {
-                if (event.propertyName === 'top') {
-                    heart.parentNode.removeChild(heart);
-                    heart.removeEventListener('transitionend', removeHeart);
-                }
-            }
-
-            heart.addEventListener('transitionend', removeHeart);
-        }, 100);
-    }
+// Initialize the GL context
+var gl = canvas.getContext('webgl');
+if(!gl){
+  console.error("Unable to initialize WebGL.");
 }
 
-customElements.define('valentine-hearts', ValentineHearts);
+//Time
+var time = 0.0;
+
+//************** Shader sources **************
+
+var vertexSource = `
+attribute vec2 position;
+void main() {
+	gl_Position = vec4(position, 0.0, 1.0);
+}
+`;
+
+var fragmentSource = `
+precision highp float;
+
+uniform float width;
+uniform float height;
+vec2 resolution = vec2(width, height);
+
+uniform float time;
+
+#define POINT_COUNT 8
+
+vec2 points[POINT_COUNT];
+const float speed = -0.5;
+const float len = 0.25;
+float intensity = 0.9;
+float radius = 0.015;
+
+//https://www.shadertoy.com/view/MlKcDD
+//Signed distance to a quadratic bezier
+float sdBezier(vec2 pos, vec2 A, vec2 B, vec2 C){    
+	vec2 a = B - A;
+	vec2 b = A - 2.0*B + C;
+	vec2 c = a * 2.0;
+	vec2 d = A - pos;
+
+	float kk = 1.0 / dot(b,b);
+	float kx = kk * dot(a,b);
+	float ky = kk * (2.0*dot(a,a)+dot(d,b)) / 3.0;
+	float kz = kk * dot(d,a);      
+
+	float res = 0.0;
+
+	float p = ky - kx*kx;
+	float p3 = p*p*p;
+	float q = kx*(2.0*kx*kx - 3.0*ky) + kz;
+	float h = q*q + 4.0*p3;
+
+	if(h >= 0.0){ 
+		h = sqrt(h);
+		vec2 x = (vec2(h, -h) - q) / 2.0;
+		vec2 uv = sign(x)*pow(abs(x), vec2(1.0/3.0));
+		float t = uv.x + uv.y - kx;
+		t = clamp( t, 0.0, 1.0 );
+
+		// 1 root
+		vec2 qos = d + (c + b*t)*t;
+		res = length(qos);
+	}else{
+		float z = sqrt(-p);
+		float v = acos( q/(p*z*2.0) ) / 3.0;
+		float m = cos(v);
+		float n = sin(v)*1.732050808;
+		vec3 t = vec3(m + m, -n - m, n - m) * z - kx;
+		t = clamp( t, 0.0, 1.0 );
+
+		// 3 roots
+		vec2 qos = d + (c + b*t.x)*t.x;
+		float dis = dot(qos,qos);
+        
+		res = dis;
+
+		qos = d + (c + b*t.y)*t.y;
+		dis = dot(qos,qos);
+		res = min(res,dis);
+		
+		qos = d + (c + b*t.z)*t.z;
+		dis = dot(qos,qos);
+		res = min(res,dis);
+
+		res = sqrt( res );
+	}
+    
+	return res;
+}
+
+
+//http://mathworld.wolfram.com/HeartCurve.html
+vec2 getHeartPosition(float t){
+	return vec2(16.0 * sin(t) * sin(t) * sin(t),
+							-(13.0 * cos(t) - 5.0 * cos(2.0*t)
+							- 2.0 * cos(3.0*t) - cos(4.0*t)));
+}
+
+//https://www.shadertoy.com/view/3s3GDn
+float getGlow(float dist, float radius, float intensity){
+	return pow(radius/dist, intensity);
+}
+
+float getSegment(float t, vec2 pos, float offset, float scale){
+	for(int i = 0; i < POINT_COUNT; i++){
+		points[i] = getHeartPosition(offset + float(i)*len + fract(speed * t) * 6.28);
+	}
+    
+	vec2 c = (points[0] + points[1]) / 2.0;
+	vec2 c_prev;
+	float dist = 10000.0;
+    
+	for(int i = 0; i < POINT_COUNT-1; i++){
+		//https://tinyurl.com/y2htbwkm
+		c_prev = c;
+		c = (points[i] + points[i+1]) / 2.0;
+		dist = min(dist, sdBezier(pos, scale * c_prev, scale * points[i], scale * c));
+	}
+	return max(0.0, dist);
+}
+
+void main(){
+	vec2 uv = gl_FragCoord.xy/resolution.xy;
+	float widthHeightRatio = resolution.x/resolution.y;
+	vec2 centre = vec2(0.5, 0.5);
+	vec2 pos = centre - uv;
+	pos.y /= widthHeightRatio;
+	//Shift upwards to centre heart
+	pos.y += 0.02;
+	float scale = 0.000015 * height;
+	
+	float t = time;
+    
+	//Get first segment
+	float dist = getSegment(t, pos, 0.0, scale);
+	float glow = getGlow(dist, radius, intensity);
+    
+	vec3 col = vec3(0.0);
+    
+	//White core
+	col += 10.0*vec3(smoothstep(0.003, 0.001, dist));
+	//Pink glow
+	col += glow * vec3(0.94,0.14,0.4);
+    
+	//Get second segment
+	dist = getSegment(t, pos, 3.4, scale);
+	glow = getGlow(dist, radius, intensity);
+    
+	//White core
+	col += 10.0*vec3(smoothstep(0.003, 0.001, dist));
+	//Blue glow
+	col += glow * vec3(0.2,0.6,1.0);
+        
+	//Tone mapping
+	col = 1.0 - exp(-col);
+
+	//Output to screen
+ 	gl_FragColor = vec4(col,1.0);
+}
+`;
+
+//************** Utility functions **************
+
+window.addEventListener('resize', onWindowResize, false);
+
+function onWindowResize(){
+  canvas.width  = window.innerWidth;
+  canvas.height = window.innerHeight;
+	gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.uniform1f(widthHandle, window.innerWidth);
+  gl.uniform1f(heightHandle, window.innerHeight);
+}
+
+
+//Compile shader and combine with source
+function compileShader(shaderSource, shaderType){
+  var shader = gl.createShader(shaderType);
+  gl.shaderSource(shader, shaderSource);
+  gl.compileShader(shader);
+  if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS)){
+  	throw "Shader compile failed with: " + gl.getShaderInfoLog(shader);
+  }
+  return shader;
+}
+
+//From https://codepen.io/jlfwong/pen/GqmroZ
+//Utility to complain loudly if we fail to find the attribute/uniform
+function getAttribLocation(program, name) {
+  var attributeLocation = gl.getAttribLocation(program, name);
+  if (attributeLocation === -1) {
+  	throw 'Cannot find attribute ' + name + '.';
+  }
+  return attributeLocation;
+}
+
+function getUniformLocation(program, name) {
+  var attributeLocation = gl.getUniformLocation(program, name);
+  if (attributeLocation === -1) {
+  	throw 'Cannot find uniform ' + name + '.';
+  }
+  return attributeLocation;
+}
+
+//************** Create shaders **************
+
+//Create vertex and fragment shaders
+var vertexShader = compileShader(vertexSource, gl.VERTEX_SHADER);
+var fragmentShader = compileShader(fragmentSource, gl.FRAGMENT_SHADER);
+
+//Create shader programs
+var program = gl.createProgram();
+gl.attachShader(program, vertexShader);
+gl.attachShader(program, fragmentShader);
+gl.linkProgram(program);
+
+gl.useProgram(program);
+
+//Set up rectangle covering entire canvas 
+var vertexData = new Float32Array([
+  -1.0,  1.0, 	// top left
+  -1.0, -1.0, 	// bottom left
+   1.0,  1.0, 	// top right
+   1.0, -1.0, 	// bottom right
+]);
+
+//Create vertex buffer
+var vertexDataBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, vertexDataBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, vertexData, gl.STATIC_DRAW);
+
+// Layout of our data in the vertex buffer
+var positionHandle = getAttribLocation(program, 'position');
+
+gl.enableVertexAttribArray(positionHandle);
+gl.vertexAttribPointer(positionHandle,
+  2, 				// position is a vec2 (2 values per component)
+  gl.FLOAT, // each component is a float
+  false, 		// don't normalize values
+  2 * 4, 		// two 4 byte float components per vertex (32 bit float is 4 bytes)
+  0 				// how many bytes inside the buffer to start from
+  );
+
+//Set uniform handle
+var timeHandle = getUniformLocation(program, 'time');
+var widthHandle = getUniformLocation(program, 'width');
+var heightHandle = getUniformLocation(program, 'height');
+
+gl.uniform1f(widthHandle, window.innerWidth);
+gl.uniform1f(heightHandle, window.innerHeight);
+
+var lastFrame = Date.now();
+var thisFrame;
+
+function draw(){
+	
+  //Update time
+	thisFrame = Date.now();
+  time += (thisFrame - lastFrame)/1000;	
+	lastFrame = thisFrame;
+
+	//Send uniforms to program
+  gl.uniform1f(timeHandle, time);
+  //Draw a triangle strip connecting vertices 0-4
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+
+  requestAnimationFrame(draw);
+}
+
+draw(); 
